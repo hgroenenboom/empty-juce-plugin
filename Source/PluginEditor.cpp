d9801d2b6bb27f9fe153fe217e22ba3754a04cae
Harold Groenenboom <HAROLD_GROE@hotmail.com>
2021-02-26 13:40:27 +0100

    added resizable code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,13 +9,29 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+#include <cassert>
+
+constexpr auto width = 600.0f;
+constexpr auto height = 500.0f;
+
 //==============================================================================
 TestAudioProcessorEditor::TestAudioProcessorEditor (TestAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
+    setResizable(true, true);
+
+    auto* constrainer = getConstrainer();
+
+    // constrainer must be valid
+    assert(constrainer);
+
+    constrainer->setMinimumSize(0.5f * width, 0.5f * height);
+    constrainer->setMaximumSize(2.0f * width, 2.0f * height);
+    constrainer->setFixedAspectRatio(width / height);
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize ((int)width, (int)height);
 }
 
 TestAudioProcessorEditor::~TestAudioProcessorEditor()
@@ -37,4 +53,6 @@ void TestAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    const auto scale = static_cast<float>(getWidth()) / width;
+    //this->setTransform(juce::AffineTransform::scale(scale)); // for child components
 }
